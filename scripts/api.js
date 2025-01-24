c379fc01c11dcf35ac0d3871ee17ea7769d8dfe0
Alice <sten123465@gmail.com>
2025-01-24 19:30:51 +0100

    add- skeleton functions for api.js

--- scripts/api.js
+++ scripts/api.js
@@ -1,2 +1,25 @@
-import {API_KEY} from "./key.js";
-console.log(API_KEY);
+//file wide constants.
+import { API_KEY } from "./key.js";
+const apiEndpoint = "https://osu.ppy.sh/p/api";
+
+//api functions
+function get(path, parameters){
+
+}
+
+//map functions
+export function getBeatmaps(filters, count = 20, mapper, mode, mods = 0){
+    if(typeof(mods) === "string")
+        mods = modStringToInt(mods);
+}
+export function intToModString(mods){
+
+}
+export function modStringToInt(modString){
+
+}
+
+//user functions
+export function getUser(user){
+    
+}
