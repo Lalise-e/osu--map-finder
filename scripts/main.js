25f885b398941519a6f7d3b0102adb5d33ed9833
Alice <sten123465@gmail.com>
2025-01-27 10:21:41 +0100

    add- functions to create search fields

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+//Functions to create form fields, the fieldName is the name that will be displayed and used in html
+//filter is a function that will return true or false depending on whether a map passes it or not.
+function createRadio(fieldName, options, filter){
+    //Options should be 2 dimensional array of strings like this
+    //[[name, keyname1, keyname2], //This will result in keyname 1 & 2 to be exclusive to each other
+    //[name2, keyname3, keyname4, keyname5] // and 3, 4 & 5 to be exclusive to each other.
+
+}
+function createCheckBox(fieldName, options, filter){
+    //Options should be a 1 dimensional array of strings where each item is the name of a checkbox
+}
+function createTextBox(fieldName, filter){
+
+}
+function createRangeSlider(fieldName, min, max, step, filter){
+
+}
