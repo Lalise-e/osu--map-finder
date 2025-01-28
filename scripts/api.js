a214dae566c10f2ff5b5de7ab0caf997dfecdaae
Alice <sten123465@gmail.com>
2025-01-28 13:23:38 +0100

    add- implemented get function with rate limitations

--- scripts/api.js
+++ scripts/api.js
@@ -2,9 +2,24 @@
 import { API_KEY } from "./key.js";
 export { getBeatmaps, intToModString, modStringToInt, getUser };
 const apiEndpoint = "https://osu.ppy.sh/api";
+const rateLimit = 1000;
+const rateLimitPeriod = 60000; //1 min
+let requests = 0;
 
 //api functions
 async function get(path, parameters){
+    //todo: error handling
+    const url = new URL(path);
+    parameters.push(["k", API_KEY]);
+    parameters.forEach(element => {
+        url.searchParams.append(element[0], element[1]);
+    });
+    while(requests >= rateLimit)
+        await new Promise(res => {setTimeout(res, 100)}); //waits 100ms as to not freeze the browser
+    requests++;
+    const item = await (await fetch(url)).json();
+    setTimeout(() => {console.log(requests); requests--;}, rateLimitPeriod)
+    return item;
 }
 
 //map functions
