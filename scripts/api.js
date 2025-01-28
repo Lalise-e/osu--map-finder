90b07c688e46688d59220fed35177b915cbea2a9
Alice <sten123465@gmail.com>
2025-01-28 09:39:58 +0100

    change- export to top

--- scripts/api.js
+++ scripts/api.js
@@ -1,25 +1,24 @@
 //file wide constants.
 import { API_KEY } from "./key.js";
-const apiEndpoint = "https://osu.ppy.sh/p/api";
+export { getBeatmaps, intToModString, modStringToInt, getUser };
+const apiEndpoint = "https://osu.ppy.sh/api";
 
 //api functions
-function get(path, parameters){
-
+async function get(path, parameters){
 }
 
 //map functions
-export function getBeatmaps(filters, count = 20, mapper, mode, mods = 0){
+function getBeatmaps(filters, count = 1, mapper, mode, mods = 0){
     if(typeof(mods) === "string")
         mods = modStringToInt(mods);
-}
-export function intToModString(mods){
+function intToModString(mods){
 
 }
-export function modStringToInt(modString){
+function modStringToInt(modString){
 
 }
 
 //user functions
-export function getUser(user){
+function getUser(user){
     
 }
