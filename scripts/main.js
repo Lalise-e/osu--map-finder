fe634abb060212f516276a0bf701e023adbee44b
Alice <sten123465@gmail.com>
2025-02-02 00:33:41 +0100

    change- some wording in article tag

--- scripts/main.js
+++ scripts/main.js
@@ -92,8 +92,8 @@ async function createSetArticle(mapSet){
 </header>
 <main class="mapset-info">
     <a href="https://osu.ppy.sh/beatmapsets/${map.beatmapset_id}" target="_blank" class="mapset-title"><h3>${map.title}</h3></a>
-    <p class="mapset-artist">${map.artist}</p>
-    <p class="mapset-status">${status}</p>
+    <p class="mapset-artist">by ${map.artist}</p>
+    <p class="mapset-status">Status: ${status}</p>
     <p class="mapset-submit">Submited: <time datetime="${map.submit_date} UTC+0">${date.getUTCFullYear()}-${date.getUTCMonth() + 1 /*apparently the month is 0 indexed*/}-${date.getUTCDate()}</time></p>
     <a href="osu://s/${map.beatmapset_id}" class="mapset-download">Download!</a>
     <!-- this only works if you have supporter, I will add a second button for non-supporter downloads, it will just take some work -->
